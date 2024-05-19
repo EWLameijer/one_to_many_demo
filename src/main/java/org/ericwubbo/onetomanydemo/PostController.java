e914ed9d01315db3caa82100574617358a73d94b
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-05-19 09:13:06 +0200

    With reverse search via database

--- src/main/java/org/ericwubbo/onetomanydemo/PostController.java
+++ src/main/java/org/ericwubbo/onetomanydemo/PostController.java
@@ -2,6 +2,7 @@ package org.ericwubbo.onetomanydemo;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,7 +13,7 @@ public class PostController {
     private final PostRepository postRepository;
 
     @GetMapping
-    public List<Post> getAll() {
-        return postRepository.findAll();
+    public List<Post> getAll(@RequestParam(required = false) String author) {
+        return author == null ? postRepository.findAll() : postRepository.findByAuthorName(author);
     }
 }
--- src/main/java/org/ericwubbo/onetomanydemo/PostRepository.java
+++ src/main/java/org/ericwubbo/onetomanydemo/PostRepository.java
@@ -2,5 +2,8 @@ package org.ericwubbo.onetomanydemo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByAuthorName(String author);
 }
